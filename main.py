0805127a731c38b9dcfa652c6a552bdfca0ac9af
adam-mcdaniel <adam.mcdaniel17@gmail.com>
2019-01-03 06:02:46 -0500

    Cleaned up main.py

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ class CMD_Image(Image):
 
 
 # python main.py --show "original" --hsv --mask 27 100 100 40 255 255 --blur 1 --draw_target --bgr --show "output"
-def main(image):
+def parse(image):
     n = 0
     while True:
         if n >= len(cmd):
@@ -89,21 +89,13 @@ def main(image):
 
 cap = cv2.VideoCapture(0)
 
-n = 0
+
 while True:
-    n += 1
-    start = time.time()
-
-
     _, raw = cap.read()
-    result = main(
+    result = parse(
         CMD_Image(raw).resize((340, 240))
         )
     print(result)
-    # if n % 10 == 0:
-    #     print(
-    #         1.0 / (time.time() - start)
-    #     )
 
     if escape_key_pressed():
         break
