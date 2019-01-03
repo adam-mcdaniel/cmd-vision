681d9af40cd56a2e7af8e4b791fb70dbe5b95a03
adam-mcdaniel <adam.mcdaniel17@gmail.com>
2019-01-03 08:56:00 -0500

    Added .gitignore

--- example.sh
+++ example.sh
@@ -1,4 +1,4 @@
-python3 main.py --resize 640 480 --show "Raw Camera Data" --hsv \
+python3 main.py --resize 320 240 --show "Raw Camera Data" --hsv \
     --mask 60 40 40 90 255 255 \
     --smooth --blur 1.0 --draw_target \
     --bgr --show "Output"
